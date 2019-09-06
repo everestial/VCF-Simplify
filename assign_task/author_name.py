0d76e3187b14782269f5059c700a4d661842e76e
Kiran N' Bishwa <kirannbishwa01@gmail.com>
2019-09-06 10:17:48 -0400

    Update author_name.py
    Added contributors name

--- assign_task/author_name.py
+++ assign_task/author_name.py
@@ -4,5 +4,6 @@
 def print_author_name():
     print("    ## VCF Simplify ## : Python application for parsing VCF files.")
     print("    Author: Bishwa K. Giri")
-    print("    Contact: bkgiri@uncg.edu, kirannbishwa01@gmail.com")
+    print("    Contributors: Bhuwan Aryal")
+    print("    Contact: bkgiri@uncg.edu, kirannbishwa01@gmail.com, bhuwanaryal19@gmail.com")
     print()
